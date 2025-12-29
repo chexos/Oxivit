9cc1a9972a64991b1044e34e96ac1ca4a925f218
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2025-12-28 22:46:25 -0500

    Cambios en la función carrusel beneficios interminable se cambió la condición y se pasó a bloque de la condicional.

--- js/index.js
+++ js/index.js
@@ -610,7 +610,7 @@ function cambiarBeneficio() {
 }
 function reiniciarRotacionBeneficios() {
 	clearTimeout(rotarBeneficios);
-	rotarBeneficios = setTimeout("rotacionBeneficios()", 3000);
+	//rotarBeneficios = setTimeout("rotacionBeneficios()", 3000);
 }
 function rotacionBeneficios() {
 	verificarTransicionBenficio();
@@ -624,7 +624,7 @@ function rotacionBeneficios() {
 	let x2;
 	let x3;
 	verificarBeneficios();
-	rotarBeneficios = setTimeout("rotacionBeneficios()", 3000);
+	//rotarBeneficios = setTimeout("rotacionBeneficios()", 3000);
 	if (index.clientWidth >= 595) {
 		clearTimeout(rotarBeneficios);
 	}
@@ -724,7 +724,7 @@ function rotacionBeneficios() {
 			}
 			mainBeneficios[0].style.transition = "1s all";
 			verificarBeneficios();
-			rotarBeneficios = setTimeout("rotacionBeneficios()", 3000);
+			//rotarBeneficios = setTimeout("rotacionBeneficios()", 3000);
 			event.stopImmediatePropagation();
 		}
 	}
@@ -732,8 +732,8 @@ function rotacionBeneficios() {
 	function carruselBeneficiosInterminable() {
 		if (cb == 6) {
 			cb = 1;
-		} else if (cb == 5) {
-			cb = 0;
+		} else if (cb == 0) {
+			cb = 5;
 		}
 		mainBeneficios[0].style.transition = "0s";
 		verificarBeneficios();
